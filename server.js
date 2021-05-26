29c6d5c1671b7fcd6bb6e4bef97362bb0baa96ad
duyl97 <dl97.app@gmail.com>
2021-05-27 05:46:53 +0700

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 //require dependencies
-const logger = require('morgan');
+const path = require('path');
+const logger = require('morgan'); //create a logger middleware function
 const express = require('express');
+const { sendResponse } = require('./app/helpers');
+const { fetchAuthorProfile } = require('./app/scotch');
 
 //create an express application
 const app = express();
@@ -12,6 +15,13 @@ app.set('port', port);
 //load middle-wares
 app.use(logger('dev'));
 
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+
+app.get('/scotch/:author', (req, res, next) => {
+	const author = req.params.author;
+	sendResponse(res)(fetchAuthorProfile(author));
+});
+
 //start the server and listen on the pre-configured port
 
-app.listen(port, () => console.log('App started on port ${port}.'))
+app.listen(port, () => console.log(`App started on port ${port}.`));
